b8e0df60d87ad403f70024265410292c5f5b2990
Amir Ghedira <amirghedira06@gmail.com>
2022-05-13 00:31:58 +0100

    adding versioning

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,12 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 )
 
+func HandleVersion(versionCmd *flag.FlagSet, version string) {
 
-func HandleHelp(helpCmd *flag.FlagSet){
+	versionCmd.Parse(os.Args[2:])
+	fmt.Printf("cwc-cli/%v %v %v\n", version, runtime.GOOS, runtime.GOARCH)
+}
 
-	helpCmd.Parse(os.Args[2: ])
+func HandleHelp(helpCmd *flag.FlagSet) {
+
+	helpCmd.Parse(os.Args[2:])
 	fmt.Printf("cwc: available commands:\n\n\n")
 	fmt.Printf("- create \n")
 	fmt.Printf("  create a new instance\n\n")
@@ -23,30 +29,29 @@ func HandleHelp(helpCmd *flag.FlagSet){
 	fmt.Printf("- configure \n")
 	fmt.Printf("  configure your default settings\n\n")
 
-
 }
 
-func HandleGet(getCmd *flag.FlagSet,all *bool, id *string ){
+func HandleGet(getCmd *flag.FlagSet, all *bool, id *string) {
 
-	getCmd.Parse(os.Args[2: ])
+	getCmd.Parse(os.Args[2:])
 	if !*all && *id == "" {
 		fmt.Println("id is required or specify --all to get all instances.")
 		getCmd.PrintDefaults()
 		os.Exit(1)
 	}
 	client := client.NewClient()
-	if *all{
+	if *all {
 
-		projects ,err := client.GetAll()
+		projects, err := client.GetAll()
 
 		if err != nil {
-			fmt.Printf("failed: %s\n",err)
+			fmt.Printf("failed: %s\n", err)
 			os.Exit(1)
 		}
 
 		fmt.Printf("ID\tname\tstatus\tsize\tenvironment\tpublic ip\tgitlab url\n")
-		for _,project := range *projects{
-			fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\n",project.Id,project.Name,project.Status,project.Instance_type,project.Environment,project.Ip_address,project.Gitlab_url)
+		for _, project := range *projects {
+			fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\n", project.Id, project.Name, project.Status, project.Instance_type, project.Environment, project.Ip_address, project.Gitlab_url)
 
 		}
 
@@ -54,57 +59,57 @@ func HandleGet(getCmd *flag.FlagSet,all *bool, id *string ){
 	}
 
 	if *id != "" {
-		project ,err := client.GetProject(*id)
+		project, err := client.GetProject(*id)
 		if err != nil {
-			fmt.Printf("failed: %s\n",err)
+			fmt.Printf("failed: %s\n", err)
 			os.Exit(1)
 		}
 		fmt.Printf("ID\tname\tstatus\tsize\tenvironment\tpublic ip\tgitlab url\n")
-		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\n",project.Id,project.Name,project.Status,project.Instance_type,project.Environment,project.Ip_address,project.Gitlab_url)
+		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\n", project.Id, project.Name, project.Status, project.Instance_type, project.Environment, project.Ip_address, project.Gitlab_url)
 
 		return
 	}
 }
 
-func HandleLogin(loginCmd *flag.FlagSet, email *string,password *string ){
+func HandleLogin(loginCmd *flag.FlagSet, email *string, password *string) {
 
-	loginCmd.Parse(os.Args[2: ])
-	if  *email == "" || *password=="" {
+	loginCmd.Parse(os.Args[2:])
+	if *email == "" || *password == "" {
 		fmt.Println("email and password are required to login")
 		loginCmd.PrintDefaults()
 		os.Exit(1)
 	}
 	client := client.NewClient()
 
-		err := client.UserLogin(*email,*password)
-		if err != nil {
-			fmt.Printf("failed: %s\n",err)
-			os.Exit(1)
-		}
+	err := client.UserLogin(*email, *password)
+	if err != nil {
+		fmt.Printf("failed: %s\n", err)
+		os.Exit(1)
+	}
 	fmt.Printf("You are successfully logged in\n")
 }
 
-func HandleConfigure(configureCmd *flag.FlagSet, region *bool){
+func HandleConfigure(configureCmd *flag.FlagSet, region *bool) {
 
-	configureCmd.Parse(os.Args[2: ])
-	if  !*region {
+	configureCmd.Parse(os.Args[2:])
+	if !*region {
 		fmt.Println("cwc: flag is missing")
 		configureCmd.PrintDefaults()
 		os.Exit(1)
 	}
-	if len(os.Args)<4{
+	if len(os.Args) < 4 {
 		fmt.Println("cwc: invalid arguments")
 		fmt.Println("cwc configure <command> <subcommand> <value>")
 		fmt.Println("<command>: -region")
 		fmt.Println("<subcommand>: get / set")
 		os.Exit(1)
 	}
-	subSubCommmand:= os.Args[3]
-	switch subSubCommmand{
+	subSubCommmand := os.Args[3]
+	switch subSubCommmand {
 
 	case "set":
-		region_value:=os.Args[4]
-		if region_value == ""{
+		region_value := os.Args[4]
+		if region_value == "" {
 			fmt.Println("cwc: region value is missing")
 			configureCmd.PrintDefaults()
 			os.Exit(1)
@@ -114,86 +119,83 @@ func HandleConfigure(configureCmd *flag.FlagSet, region *bool){
 		possible_regions[1] = "fr-par-2"
 		possible_regions[2] = "nl-ams-1"
 		possible_regions[3] = "pl-waw-1"
-		
-		if !stringInSlice(region_value,possible_regions){
+
+		if !stringInSlice(region_value, possible_regions) {
 			fmt.Println("cwc: invalid region value")
 			os.Exit(1)
 
 		}
 
 		client.SetDefaultRegion(region_value)
-		fmt.Printf("Default region = %v\n",region_value)
+		fmt.Printf("Default region = %v\n", region_value)
 	case "get":
-		region:=client.GetDefaultRegion()
-		fmt.Printf("Default region = %v\n",region)
-	default : 
+		region := client.GetDefaultRegion()
+		fmt.Printf("Default region = %v\n", region)
+	default:
 		fmt.Printf("cwc: option not found")
 	}
 
 }
 
-func HandleDelete(deleteCmd *flag.FlagSet, id *string){
+func HandleDelete(deleteCmd *flag.FlagSet, id *string) {
 
-	deleteCmd.Parse(os.Args[2: ])
-	if  *id == "" {
+	deleteCmd.Parse(os.Args[2:])
+	if *id == "" {
 		fmt.Println("id is required to delete your instance")
 		deleteCmd.PrintDefaults()
 		os.Exit(1)
 	}
 	client := client.NewClient()
 
-		err := client.DeleteProject(*id)
-		if err != nil {
-			fmt.Printf("failed: %s\n",err)
-			os.Exit(1)
-		}
-	fmt.Printf("Project %v successfully deleted\n",*id)
+	err := client.DeleteProject(*id)
+	if err != nil {
+		fmt.Printf("failed: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Project %v successfully deleted\n", *id)
 }
-func ValidateProject(createCmd *flag.FlagSet,name *string,env *string){
+func ValidateProject(createCmd *flag.FlagSet, name *string, env *string) {
 
 	if *name == "" || *env == "" {
 		createCmd.PrintDefaults()
 		os.Exit(1)
 	}
 }
-func HandleAdd(createCmd *flag.FlagSet,name *string,email *string,env *string,instance_type *string){
-	createCmd.Parse(os.Args[2: ])
-	ValidateProject(createCmd,name,env)
+func HandleAdd(createCmd *flag.FlagSet, name *string, email *string, env *string, instance_type *string) {
+	createCmd.Parse(os.Args[2:])
+	ValidateProject(createCmd, name, env)
 	client := client.NewClient()
-	created_project,err := client.AddProject(*name, *instance_type,*env, *email)
+	created_project, err := client.AddProject(*name, *instance_type, *env, *email)
 	if err != nil {
-		fmt.Printf("failed: %s\n",err)
+		fmt.Printf("failed: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("ID\tname\tstatus\tsize\tenvironment\tgitlab url\n")
-	fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\n",created_project.Id,created_project.Name,created_project.Status,created_project.Instance_type,created_project.Environment,created_project.Gitlab_url)
-
+	fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\n", created_project.Id, created_project.Name, created_project.Status, created_project.Instance_type, created_project.Environment, created_project.Gitlab_url)
 
 }
 
-func HandleUpdate(updateCmd *flag.FlagSet,id *string,status *string){
-	updateCmd.Parse(os.Args[2: ])
-	if *id == "" || *status =="" {
+func HandleUpdate(updateCmd *flag.FlagSet, id *string, status *string) {
+	updateCmd.Parse(os.Args[2:])
+	if *id == "" || *status == "" {
 		fmt.Println("id and status are required")
 		updateCmd.PrintDefaults()
 		os.Exit(1)
 	}
 	client := client.NewClient()
-	err := client.UpdateProject(*id,*status)
+	err := client.UpdateProject(*id, *status)
 	if err != nil {
-		fmt.Printf("failed: %s\n",err)
+		fmt.Printf("failed: %s\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("Project %v successfully updated\n", *id)
 
-
 }
-
 
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
-		return true
+			return true
 		}
 	}
 	return false
--- main.go
+++ main.go
@@ -8,45 +8,40 @@ import (
 	"os"
 )
 
+var Version = "dev"
 
+func main() {
 
-func main(){
+	fmt.Println("version " + Version)
 
+	helpCmd := flag.NewFlagSet("help", flag.ExitOnError)
+	versionCmd := flag.NewFlagSet("version", flag.ExitOnError)
 
-	helpCmd := flag.NewFlagSet("help",flag.ExitOnError)
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+	getAll := getCmd.Bool("all", false, "Get all instances")
+	getById := getCmd.String("id", "", "Get instance by ID")
 
-	getCmd := flag.NewFlagSet("get",flag.ExitOnError)
-	getAll:= getCmd.Bool("all",false,"Get all instances")
-	getById:= getCmd.String("id", "" ,"Get instance by ID")
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	addProjectName := createCmd.String("name", "", "The Project name")
+	addEmail := createCmd.String("email", "", "The email address associeted to the project")
+	addEnvironment := createCmd.String("env", "", "The environment of the project (code, wpaas)")
+	addInstanceType := createCmd.String("instance_type", "", "The instance size (DEV1-S, DEV1-M, DEV1-L, DEV1-XL)")
 
+	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+	deleteById := deleteCmd.String("id", "", "Target instance ID")
 
- 	createCmd := flag.NewFlagSet("create",flag.ExitOnError)
-	addProjectName:= createCmd.String("name", "" ,"The Project name")
-	addEmail:= createCmd.String("email", "" ,"The email address associeted to the project")
-	addEnvironment:= createCmd.String("env", "" ,"The environment of the project (code, wpaas)")
-	addInstanceType:= createCmd.String("instance_type", "" ,"The instance size (DEV1-S, DEV1-M, DEV1-L, DEV1-XL)")
+	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+	updateId := updateCmd.String("id", "", "Target instance ID")
+	updateStatus := updateCmd.String("status", "", "Instance status (poweroff, poweron, reboot)")
 
+	loginCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+	loginEmail := loginCmd.String("u", "", "Account email")
+	loginPassword := loginCmd.String("p", "", "Account password")
 
-	deleteCmd := flag.NewFlagSet("delete",flag.ExitOnError)
-	deleteById:= deleteCmd.String("id", "" ,"Target instance ID")
+	configureCmd := flag.NewFlagSet("configure", flag.ExitOnError)
+	configureRegionCmd := configureCmd.Bool("region", false, "Configure the default region")
 
-
-	updateCmd := flag.NewFlagSet("update",flag.ExitOnError)
-	updateId:= updateCmd.String("id", "" ,"Target instance ID")
-	updateStatus:= updateCmd.String("status", "" ,"Instance status (poweroff, poweron, reboot)")
-
-	loginCmd := flag.NewFlagSet("delete",flag.ExitOnError)
-	loginEmail:= loginCmd.String("u", "" ,"Account email")
-	loginPassword:= loginCmd.String("p", "" ,"Account password")
-
-
-
-	configureCmd := flag.NewFlagSet("configure",flag.ExitOnError)
-	configureRegionCmd:= configureCmd.Bool("region", false ,"Configure the default region")
-
-
-
-	if len(os.Args)<2 {
+	if len(os.Args) < 2 {
 		fmt.Println("usage: cwc <command> [parameters]")
 		fmt.Printf("To see help text, you can run:\n\n")
 		// fmt.Println("cwc help")
@@ -56,27 +51,27 @@ func main(){
 		return
 	}
 
-	switch os.Args[1]{
+	switch os.Args[1] {
 
 	case "get":
-		handlers.HandleGet(getCmd,getAll,getById)
+		handlers.HandleGet(getCmd, getAll, getById)
 	case "create":
-		handlers.HandleAdd(createCmd,addProjectName,addEmail,addEnvironment,addInstanceType)
+		handlers.HandleAdd(createCmd, addProjectName, addEmail, addEnvironment, addInstanceType)
 	case "delete":
-		handlers.HandleDelete(deleteCmd,deleteById)
+		handlers.HandleDelete(deleteCmd, deleteById)
 	case "update":
-		handlers.HandleUpdate(updateCmd,updateId,updateStatus)
+		handlers.HandleUpdate(updateCmd, updateId, updateStatus)
 
 	case "login":
-		handlers.HandleLogin(loginCmd,loginEmail,loginPassword)
+		handlers.HandleLogin(loginCmd, loginEmail, loginPassword)
 	case "configure":
-		handlers.HandleConfigure(configureCmd,configureRegionCmd)
+		handlers.HandleConfigure(configureCmd, configureRegionCmd)
 	case "help":
 		handlers.HandleHelp(helpCmd)
-	default : 
+	case "--version":
+		handlers.HandleVersion(versionCmd, Version)
+	default:
 		fmt.Printf("cwc: command not found")
 	}
 
-
 }
-
