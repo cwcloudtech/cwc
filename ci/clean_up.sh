2b30a016cab098026e8672e612fe6d3ee7eddaeb
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-10-13 13:35:19 +0100

    adding cleanup stage

---
+++ ci/clean_up.sh
@@ -0,0 +1,12 @@
+
+i=0
+for project in $(curl -X GET --header "PRIVATE-TOKEN:${GITLAB_TOKEN}"  ${GITLAB_HOST}/api/v4/projects/${GITLAB_PROJECT}/releases | jq -r '.[] | @base64'); do
+    i=$(( i + 1 ))
+    _jq() {
+     echo ${project} | base64 --decode | jq -r ${1}
+    }
+    if [ $i -gt 5 ]
+     then
+      curl -X DELETE --header "PRIVATE-TOKEN:${GITLAB_TOKEN}" https://gitlab.comwork.io/api/v4/projects/${GITLAB_PROJECT}/releases/$(_jq '.tag_name') >/dev/null
+    fi
+done
---
+++ ci/release.sh
@@ -0,0 +1,7 @@
+docker run --rm --privileged \
+      -v $PWD:/go/src/gitlab.com/goreleaser/cwc \
+      -w /go/src/gitlab.com/goreleaser/cwc \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -e $DOCKER_USERNAME -e $DOCKER_PASSWORD -e $DOCKER_REGISTRY  \
+      -e $GITLAB_TOKEN \
+      goreleaser/goreleaser release --rm-dist
