6ec7455d58f034e4880db0f43db0a25dd2f0d607
Idriss Neumann <idriss.neumann@comwork.io>
2023-12-18 14:03:07 +0100

    Add pretty and json format for DNS zones

--- handlers/user/dns_zones.go
+++ handlers/user/dns_zones.go
@@ -2,15 +2,19 @@ package user
 
 import (
 	"cwc/client"
+	"cwc/config"
 	"cwc/utils"
-	"fmt"
 )
 
-func HandleListDnsZones() {
+func HandleListDnsZones(pretty *bool) {
 	dns_zones, err := client.GetDnsZones()
 	utils.ExitIfError(err)
 
-	for _, dns_zone := range dns_zones.Zones {
-		fmt.Printf("%v\n", dns_zone)
+	if config.IsPrettyFormatExpected(pretty) {
+		utils.PrintPrettyArray("DNS zones available", dns_zones.Zones)
+	} else if config.GetDefaultFormat() == "json" {
+		utils.PrintJson(dns_zones)
+	} else {
+		utils.PrintArray(dns_zones.Zones)
 	}
 }
