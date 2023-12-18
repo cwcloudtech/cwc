e9d393738b7774541e76ffc02ed7680b849fd8e8
Idriss Neumann <idriss.neumann@comwork.io>
2023-12-18 14:38:40 +0100

    Add admin bucket pretty ls option

--- cmd/admin/bucket/ls/ls.go
+++ cmd/admin/bucket/ls/ls.go
@@ -20,7 +20,7 @@ var LsCmd = &cobra.Command{
 This command takes no arguments`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if utils.IsBlank(bucketId) {
-			admin.HandleGetBuckets()
+			admin.HandleGetBuckets(&pretty)
 		} else {
 			admin.HandleGetBucket(&bucketId, &pretty)
 		}
--- handlers/admin/bucket.go
+++ handlers/admin/bucket.go
@@ -5,6 +5,9 @@ import (
 	"cwc/config"
 	"cwc/utils"
 	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func HandleAddBucket(user_email *string, name *string, reg_type *string) {
@@ -41,14 +44,16 @@ func HandleUpdateBucket(id *string, email *string) {
 	fmt.Printf("Bucket %v successfully updated\n", *id)
 }
 
-func HandleGetBuckets() {
+func HandleGetBuckets(pretty *bool) {
 	c, err := admin.NewClient()
 	utils.ExitIfError(err)
 
 	buckets, err := c.GetAllBuckets()
 	utils.ExitIfError(err)
 
-	if config.GetDefaultFormat() == "json" {
+	if config.IsPrettyFormatExpected(pretty) {
+		displayBucketsAsTable(*buckets)
+	} else if config.GetDefaultFormat() == "json" {
 		utils.PrintJson(buckets)
 	} else {
 		utils.PrintMultiRow(admin.Bucket{}, *buckets)
@@ -69,4 +74,26 @@ func HandleGetBucket(id *string, pretty *bool) {
 	} else {
 		utils.PrintRow(*bucket)
 	}
+}
+
+func displayBucketsAsTable(buckets []admin.Bucket) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ID", "Name", "Type", "Endpoint", "Region", "Created at"})
+
+	if len(buckets) == 0 {
+		fmt.Println("No buckets found")
+	} else {
+		for _, bucket := range buckets {
+			table.Append([]string{
+				fmt.Sprintf("%d", bucket.Id),
+				bucket.Name,
+				bucket.Type,
+				bucket.Endpoint,
+				bucket.Region,
+				bucket.CreatedAt,
+			})
+		}
+	}
+
+	table.Render()
 }
