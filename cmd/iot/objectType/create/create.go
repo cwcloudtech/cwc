5d928d1ad70f57888f9ebd9c2228569295f8c387
Ayoub Abidi <mrayoubabidi@gmail.com>
2024-04-19 15:47:42 +0100

    fix: typo

--- cmd/iot/objectType/create/create.go
+++ cmd/iot/objectType/create/create.go
@@ -15,28 +15,31 @@ var (
 	pretty      bool = false
 )
 
-var CreateCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create an object type in the cloud",
-	Long:  "This command lets you create an object type in the cloud.",
-	Run: func(cmd *cobra.Command, args []string) {
-		created_objectType, err := user.PrepareAddObjectType(&objectType, &interactive)
-		utils.ExitIfError(err)
-		user.HandleAddObjectType(created_objectType, &pretty)
-	},
-}
+var CreateCmd *cobra.Command
 
 func init() {
+	CreateCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create an object type in the cloud",
+		Long:  "This command lets you create an object type in the cloud.",
+		Run:   createCmdRun,
+	}
+
 	CreateCmd.Flags().StringVarP(&objectType.Content.Name, "name", "n", "", "Name of the object type")
 	CreateCmd.Flags().BoolVar(&objectType.Content.Public, "public", false, "Is the object type public?")
 	CreateCmd.Flags().StringVarP(&objectType.Content.DecodingFunction, "decoding_function", "d", "", "Decoding function of the object type")
 	CreateCmd.Flags().StringSliceVarP(&objectType.Content.Triggers, "triggers", "t", []string{}, "Triggers of the object type")
 	CreateCmd.Flags().BoolVarP(&interactive, "interactive", "i", false, "Interactive mode (optional)")
+}
 
+func createCmdRun(cmd *cobra.Command, args []string) {
 	if !interactive {
 		err := CreateCmd.MarkFlagRequired("decoding_function")
 		if nil != err {
 			fmt.Println(err)
 		}
 	}
+	created_objectType, err := user.PrepareAddObjectType(&objectType, &interactive)
+	utils.ExitIfError(err)
+	user.HandleAddObjectType(created_objectType, &pretty)
 }
