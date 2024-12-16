9768537fa7eb9a924c279c5dbf62242632ad8e47
Ayoub Abidi <mrayoubabidi@gmail.com>
2024-12-16 15:32:44 +0100

    fix: add interactive configuration for the bootstrap command (#1220)

--- cmd/bootstrap/bootstrap.go
+++ cmd/bootstrap/bootstrap.go
@@ -1,11 +1,13 @@
 package bootstrap
 
 import (
-	"github.com/spf13/cobra"
-
 	"cwc/cmd/bootstrap/pfw"
 	"cwc/cmd/bootstrap/uninstall"
+	"cwc/env"
 	"cwc/handlers/user"
+	"cwc/utils"
+	"fmt"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -38,28 +40,66 @@ func init() {
 	BootstrapCmd.Flags().BoolVarP(&keepDir, "keep-dir", "k", false, "Keep the local helm directory")
 	BootstrapCmd.Flags().BoolVarP(&recreateNs, "recreate-ns", "d", false, "Recreate the namespace")
 	BootstrapCmd.Flags().BoolVarP(&openshift, "openshift", "o", false, "Use openshift cli instead of kubectl")
-	BootstrapCmd.Flags().StringArrayVarP(&otherValues, "value", "p", []string{}, `Values to override other configurations (e.g. --value key=value --value key2=value2)
-
-Example:
-  All applications are enabled by default. To disable some applications, use this format:
-
-    -p applicationName.enabled=false
-
-
-  Example:
-    cwc bootstrap -p scheduler.enabled=false \
-                  -p consumer.enabled=false
-	`)
+	BootstrapCmd.Flags().StringArrayVarP(&otherValues, "value", "p", []string{}, `Values to override other configurations (e.g. --value key=value --value key2=value2)`)
 
 	configureCmd := &cobra.Command{
-		Use:   "configure [flags]",
+		Use:   "configure",
 		Short: "Bootstrap CWCloud with custom repository configuration",
-		Long: `Bootstrap CWCloud installation with temporary repository configuration overrides.
-This command allows you to specify custom repository URL, directory, and branch for a single deployment.`,
-		Example: `  cwc bootstrap configure --repo-url=https://custom-repo.git --branch=dev -r my-release -n my-namespace
-  cwc bootstrap configure --repo-url=https://gitlab.alternative.io/helm.git --directory=./custom-helm --branch=feature-123
-  cwc bootstrap configure --repo-url=https://custom-repo.git --username=myuser --password=mypassword`,
+		Long: `Bootstrap CWCloud installation with temporary repository configuration overrides. 
+		This command allows you to specify custom repository URL, directory, and branch for a single deployment.
+
+		You can either:
+		1. Use flags to specify all values directly
+		2. Run without flags for an interactive prompt`,
+		Example: `  # Interactive mode:
+		cwc bootstrap configure
+
+		# Flag-based mode:
+		cwc bootstrap configure --repo-url=https://custom-repo.git --branch=dev
+		cwc bootstrap configure --repo-url=https://gitlab.alternative.io/helm.git --branch=feature-123
+		cwc bootstrap configure --repo-url=https://custom-repo.git --username=myuser --password=mypassword`,
 		Run: func(cmd *cobra.Command, args []string) {
+			defaultRepoURL := env.REPO_URL
+			defaultBranch := env.BRANCH
+
+			if !cmd.Flags().Changed("repo-url") && !cmd.Flags().Changed("branch") &&
+				!cmd.Flags().Changed("username") && !cmd.Flags().Changed("password") {
+
+				fmt.Printf("Repository URL [%s]: ", defaultRepoURL)
+				userRepoURL := utils.PromptUserForValue()
+				if utils.IsNotBlank(userRepoURL) {
+					tempRepoURL = userRepoURL
+				} else {
+					tempRepoURL = defaultRepoURL
+				}
+
+				fmt.Printf("Branch [%s]: ", defaultBranch)
+				userBranch := utils.PromptUserForValue()
+				if utils.IsNotBlank(userBranch) {
+					tempBranch = userBranch
+				} else {
+					tempBranch = defaultBranch
+				}
+
+				if tempRepoURL != defaultRepoURL {
+					fmt.Print("Username (optional): ")
+					tempUsername = utils.PromptUserForValue()
+
+					if utils.IsNotBlank(tempUsername) {
+						fmt.Print("Password (optional): ")
+						tempPassword = utils.PromptUserForValue()
+					}
+				}
+
+			} else {
+				if !cmd.Flags().Changed("repo-url") {
+					tempRepoURL = defaultRepoURL
+				}
+				if !cmd.Flags().Changed("branch") {
+					tempBranch = defaultBranch
+				}
+			}
+
 			tempConfig := &user.RepoConfig{
 				RepoURL:  tempRepoURL,
 				Branch:   tempBranch,
