b29eab2fb32fbf19019a6c3391e73251d0339666
Idriss Neumann <idriss.neumann@comwork.io>
2026-01-02 10:21:01 +0100

    Add contact form struct for admin

--- admin/entities.go
+++ admin/entities.go
@@ -463,6 +463,19 @@ type Monitor struct {
 	Hash               string             `json:"hash"`
 }
 
+type ContactForm struct {
+	Id            string `json:"id"`
+	Name          string `json:"name"`
+	Hash          string `json:"hash"`
+	UserId        int    `json:"user_id"`
+	MailFrom      string `json:"mail_from"`
+	MailTo        string `json:"mail_to"`
+	CopyrightName string `json:"copyright_name"`
+	LogoUrl       string `json:"logo_url"`
+	CreatedAt     string `json:"created_at"`
+	Updated_at    string `json:"updated_at"`
+}
+
 type AIAdapterHeader struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
