1b2e8424cb39a8a4c451a02c84a226e700a36d1c
Idriss Neumann <idriss.neumann@comwork.io>
2026-06-02 16:22:43 +0100

    Force push

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -4,6 +4,6 @@ REPO_PATH="${PROJECT_HOME}/cwc/"
 
 cd "${REPO_PATH}"
 git reset --hard origin/main
-git push github main 
-git push pgitlab main
+git push -f github main
+git push -f pgitlab main
 exit 0
