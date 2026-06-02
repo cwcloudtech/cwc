5574a6a50981f782536fa8df6a12cc0d24cebf00
Idriss Neumann <idriss.neumann@comwork.io>
2026-06-02 16:05:53 +0100

    Change mirror script

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -2,7 +2,8 @@
 
 REPO_PATH="${PROJECT_HOME}/cwc/"
 
-cd "${REPO_PATH}" && git pull origin main || :
+cd "${REPO_PATH}"
+git reset --hard origin/main
 git push github main 
 git push pgitlab main
 exit 0
